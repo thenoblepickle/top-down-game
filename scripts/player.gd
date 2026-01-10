2aa09d7aa4a4b01c88500a10399be8c4d3a63674
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-01-10 23:36:15 +0100

    Tried adding assets

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed : float = 400
+@export var speed : float = 150
 
 func _physics_process(_delta: float) -> void:
 	var input_vector = Input.get_vector("left", "right", "up", "down")
